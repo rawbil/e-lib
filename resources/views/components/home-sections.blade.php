58dd344ca8d320ac2fb99d617cadc3ac31779f3d
hackTivist <bildadsimiyu6@gmail.com>
2025-07-22 19:36:00 +0300

    feat: Landing page(Tweakins rem)

---
+++ resources/views/components/home-sections.blade.php
@@ -0,0 +1,3 @@
+<section {{ $attributes->merge(['class' => 'bg-dark text-light mt-10 rounded p-5 py-10 pb-20 max-1500px:px-3 shadow shadow-slate-800 max-600px:rounded-none']) }} ">
+    {{ $slot }}
+</section>
