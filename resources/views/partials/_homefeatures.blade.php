7fce68e500d84b6811a195eb8e78cd6fa11af446
hackTivist <bildadsimiyu6@gmail.com>
2025-07-22 23:25:36 +0300

    feat: Add search input in catalogue

--- resources/views/partials/_homefeatures.blade.php
+++ resources/views/partials/_homefeatures.blade.php
@@ -17,7 +17,7 @@
             <i class="fa-solid fa-magnifying-glass text-5xl max-1500px:text-3xl group-hover:scale-75 duration-300"></i>
             <span class="text-light/80 text-lg max-1500px:text-base group-hover:text-light transition-all duration-300">Search for Books</span>
         </a>
-        <a href="/library-catalogue/?tag=new"
+        <a href="/library-catalogue?tag=new"
             class="flex flex-col items-center 1500px:w-[320px] max-1500px:w-[280px] max-1360px:w-[260px] max-1230px:w-[240px] max-1150px:w-[200px] max-890px:w-[100%] flex-wrap h-[200px] max-890px:h-[180px] bg-light/20 justify-center rounded shadow-sm shadow-light/50 cursor-pointer gap-5 group">
             <i class="fa-solid fa-bell text-5xl max-1500px:text-3xl group-hover:scale-75 transition-all duration-300"></i>
             <span class="text-light/80 text-lg max-1500px:text-base group-hover:text-light transition-all duration-300">New Arrivals</span>
---
+++ resources/views/partials/_librarySearch.blade.php
@@ -0,0 +1,11 @@
+<form action="/library-catalogue" class="max-600px:mx-1">
+    <label for="search" class="text-black/70">Search for (title, author, keywords or category)</label>
+    <article class=" shadow shadow-dark flex items-center rounded relative ">
+        <input type="search" name="search" id="search"
+            class="w-full border-none focus:ring-0 focus:outline-none outline-none bg-transparent py-3 placeholder:text-start placeholder:pl-5"
+            placeholder="...Search for a book">
+        <button type="submit" class="px-2 py-2 absolute left-1">
+            <i class="fa-solid fa-magnifying-glass text-lg"></i>
+        </button>
+    </article>
+</form>
