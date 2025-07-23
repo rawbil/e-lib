0cf62c4f6b3db3e99bf87a608ec64a0ecf843b10
hackTivist <bildadsimiyu6@gmail.com>
2025-07-23 11:08:44 +0300

    feat: Implement CatalogueController and Catalogue model; enhance navbar and search input styles

--- resources/views/partials/_librarySearch.blade.php
+++ resources/views/partials/_librarySearch.blade.php
@@ -2,7 +2,7 @@
     <label for="search" class="text-black/70">Search for (title, author, keywords or category)</label>
     <article class=" shadow shadow-dark flex items-center rounded relative ">
         <input type="search" name="search" id="search"
-            class="w-full border-none focus:ring-0 focus:outline-none outline-none bg-transparent py-3 placeholder:text-start placeholder:pl-5"
+            class="w-full border-none focus:ring-0 focus:outline-none outline-none bg-transparent py-3 placeholder:text-start placeholder:pl-8 pl-8"
             placeholder="...Search for a book">
         <button type="submit" class="px-2 py-2 absolute left-1">
             <i class="fa-solid fa-magnifying-glass text-lg"></i>
