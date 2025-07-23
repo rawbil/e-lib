90024c6cd1b45285b0bab3e3612b3d0239939f31
hackTivist <bildadsimiyu6@gmail.com>
2025-07-23 11:08:44 +0300

    feat: Implement CatalogueController and Catalogue model; enhance navbar and search input styles

---
+++ app/Http/Controllers/CatalogueController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class CatalogueController extends Controller
+{
+    public static function make() {
+        return view('');
+    }
+}
